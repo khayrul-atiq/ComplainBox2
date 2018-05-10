4e41f4da927b0dcb6a06bb862c0376fe97b9074a
khayrul-atiq <khayrul.atiq@gmail.com>
2018-05-10 13:31:12 +0600

    user profile lau=yout add

--- app/src/main/java/com/example/olife/complainbox2/MyProfile.java
+++ app/src/main/java/com/example/olife/complainbox2/MyProfile.java
@@ -4,12 +4,22 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyProfile extends Fragment {
+
+    private int previousTab;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,5 +33,55 @@ public class MyProfile extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("My Profile");
+
+        //replaceFragment(new ProfileInformation(),0);
+
+
+        ViewPager viewPager = getActivity().findViewById(R.id.viewPagerContainer);
+        setupViewPager(viewPager);
+
+
+        TabLayout tabLayout = getActivity().findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
+
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
+        adapter.addFragment(new ProfileInformation(), "Profile");
+        adapter.addFragment(new ProfileAcivity(),"Activity");
+
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
 }
